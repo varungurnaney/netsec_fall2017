5641f13e35e24e59abc0c5152646ae1f07420e27
varungurnaney <varungurnaney@gmail.com>
2017-09-14 21:52:53 -0400

    Update server.py

--- lab1_d/server.py
+++ lab1_d/server.py
@@ -47,19 +47,10 @@ def server_run():
 
 	loop = asyncio.get_event_loop()
 	# Each client connection will create a new protocol instance
-	coro = playground.getConnector().create_playground_server(ServerProtocol, 8000)
+	coro = playground.getConnector().create_playground_server(ServerProtocol,'8000')
 	server = loop.run_until_complete(coro)
+	print("Server Started at {}".format(server.sockets[0].gethostname()))
+	loop.run_forever()
 
-	# Serve requests until Ctrl+C is pressed
-	print('Serving......')
-	try:
-		loop.run_forever()
-	except KeyboardInterrupt:
-		    pass
-
-	# Close the server
-	server.close()
-	loop.run_until_complete(server.wait_closed())
-	loop.close()
 
 server_run()
