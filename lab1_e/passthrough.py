5eebeb297bc332fff4b1d4e902b9752306ca9931
varungurnaney <varungurnaney@gmail.com>
2017-09-18 14:27:50 -0400

    Create passthrough.py

---
+++ lab1_e/passthrough.py
@@ -0,0 +1,36 @@
+from playground.network.packet import PacketType
+from protocol import *
+import asyncio 
+import playground
+
+class PassThrough1(StackingProtocol)
+
+	def __init__(self, callback=None):
+	
+		self.transport = None
+		
+		
+
+	def connection_made(self, transport):
+	
+		
+
+
+	def data_received(self, data):
+
+
+
+
+
+class PassThrough2(StackingProtocol)
+
+	def __init__(self, callback=None):
+
+		
+
+	def connection_made(self, transport):
+	
+		
+
+
+	def data_received(self, data):
